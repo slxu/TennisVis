1d5809c99ef5202bb8f04e867fb65ccce594c646
slxu <slxu@cs.washington.edu>
2014-01-30 16:38:02 -0800

    craw year list

--- src/data_crawler/craw.py
+++ src/data_crawler/craw.py
@@ -1,4 +1,16 @@
 import sys
 import os
+import json
+import urllib2
+
+baseURL = "http://www.atpworldtour.com/handlers/GetDrawFilter.ajax"
+
+def read_json(url):
+  return json.loads(urllib2.urlopen(url).read())
+
+def get_year_list():
+  jsonObj=read_json(baseURL)
+  return map(lambda x:x["id"], jsonObj["yrlst"])
 
 
+print get_year_list()
