7e11e52af09f75bb069c27ca6469c54c733cdc4e
Shengliang Xu <slxu@cs.washington.edu>
2014-02-06 14:50:03 -0800

    vertical sankey flow

--- src/html/js/newSankey.js
+++ src/html/js/newSankey.js
@@ -55,16 +55,22 @@ d3.sankey = function() {
  
     function link(d) {
       var x0 = d.source.x + d.source.dx,
+          y0 = d.source.y + d.sy + d.dy /2,
           x1 = d.target.x,
-          xi = d3.interpolateNumber(x0, x1),
-          x2 = xi(curvature),
-          x3 = xi(1 - curvature),
-          y0 = d.source.y + d.sy + d.dy / 2,
-          y1 = d.target.y + d.ty + d.dy / 2;
-      return "M" + x0 + "," + y0
-           + "C" + x2 + "," + y0
-           + " " + x3 + "," + y1
+          y1 = d.target.y,
+          //xi = d3.interpolateNumber(x0, x1),
+          yi = d3.interpolateNumber(y0,y1),
+          //x2 = xi(curvature),
+          y2 = yi(curvature),
+         // x3 = xi(1 - curvature),
+         y3 = yi(1-curvature);
+          //y0 = d.source.y + d.sy + d.dy / 2,
+          //y1 = d.target.y + d.ty + d.dy / 2;
+      var returnValue= "M" + x0 + "," + y0
+           + "C" + x0 + "," + y2
+           + " " + x1 + "," + y3
            + " " + x1 + "," + y1;
+      return returnValue;
     }
  
     link.curvature = function(_) {
