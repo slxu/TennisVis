755d3d5dd738103abf4c061826dac62f1af302e6
slxu <slxu@cs.washington.edu>
2014-02-11 17:59:46 -0800

    use generated data rather than a remote json file

--- src/html/js/display.js
+++ src/html/js/display.js
@@ -6,6 +6,34 @@ var formatNumber = d3.format(",.0f"),    // zero decimal places
   format = function(d) { return formatNumber(d) + " " + units; },
   color = d3.scale.category20();
 
+
+var graph={
+"tournaments":[ "T2013_338", "T2013_5014", "T2013_747", "T2013_560" ],
+"players":[ "N409", "D643", "W367", "D683", "F401", "MC10", "BA47", "F324", "G628", "T786" ],
+"links":[
+{"source":"T2013_338","target":"N409","value":124.729},
+{"source":"T2013_338","target":"D643","value":40.597},
+{"source":"T2013_5014","target":"W367","value":30.597},
+{"source":"T2013_747","target":"F401","value":20.597},
+{"source":"T2013_747","target":"MC10","value":140.597},
+{"source":"T2013_338","target":"F324","value":230.597},
+{"source":"T2013_338","target":"G628","value":300.597},
+{"source":"T2013_560","target":"BA47","value":50.597},
+{"source":"T2013_560","target":"T786","value":48.597},
+{"source":"T2013_338","target":"D643","value":24.597},
+{"source":"T2013_747","target":"F401","value":33.597},
+{"source":"T2013_747","target":"BA47","value":45.597},
+{"source":"T2013_5014","target":"F324","value":38.597},
+{"source":"T2013_5014","target":"G628","value":97.597},
+{"source":"T2013_560","target":"N409","value":29.597},
+{"source":"T2013_5014","target":"W367","value":55.597},
+{"source":"T2013_747","target":"D683","value":60.597},
+{"source":"T2013_747","target":"F401","value":44.597},
+{"source":"T2013_5014","target":"BA47","value":70.597},
+{"source":"T2013_560","target":"N409","value":10.597},
+{"source":"T2013_5014","target":"D643","value":33.597}
+]};
+
 var svg = d3.select("svg");
 var mapgraph = d3.mapgraph();
 
@@ -32,7 +60,7 @@ d3.csv("data/geodata.csv", function(error,cities){
         d.index = i;
         playerID2Info[d.id] = d;
       });
-      d3.json("data/data1.json", function(error, graph) {
+      {
 
         var toursData = graph.tournaments;
         var playersData = graph.players;
@@ -45,7 +73,8 @@ d3.csv("data/geodata.csv", function(error,cities){
           .globalIDToPlayer(playerID2Info)
           .cityToGeo(city2Geo);
         mapgraph.update();
-      }); 
+      }
+//); 
     });
   });
 });
@@ -112,6 +141,18 @@ d3.csv("data/points.json", function(error, points) {
 //      delays = delay.group(function(d) { return Math.floor(d / 10) * 10; }),
 //      distance = flight.dimension(function(d) { return Math.min(1999, d.distance); }),
 //      distances = distance.group(function(d) { return Math.floor(d / 50) * 50; });
+
+function reduceAdd(p, v) {
+  return p + 1;
+}
+
+function reduceRemove(p, v) {
+  return p - 1;
+}
+
+function reduceInitial() {
+  return 0;
+}
 
   var charts = [
 
